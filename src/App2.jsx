5717d75689aecda38a6965609fdf6b3346274968
rygoto <ryudono08@gmail.com>
2024-05-16 01:30:06 +0900

    05152

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useContext, createContext } from 'react';
+import { useSpring, animated } from 'react-spring';
 import './App.css';
 
 const ImageContext = createContext(null);
@@ -11,6 +12,7 @@ const imageSources = [
         descriptionen: 'Map Compass is a simple app made by React and Babylonjs, using XR function to map search.',
         descriptionja: 'Map Compssはこれこれこういうアプリです',
         descriptionch: 'MapCompass是一个简单的应用程序，允许用户搜索YouTube视频并播放它们。该应用程序使用React.js和Tailwind CSS构建。',
+        descriptionko: 'Map Compass는 지도 검색을 위해 XR 기능을 사용하여 React와 Babylonjs로 만든 간단한 앱입니다.',
         url: 'https://map-compass.vercel.app/'
     },
     {
@@ -20,6 +22,7 @@ const imageSources = [
         descriptionen: 'Earth-based Youtubesearch is a music player app that allows users to search for songs and play them. The app is built using React.js and Tailwind CSS.',
         descriptionja: 'Earth-based Youtubesearchは、ユーザーが曲を検索して再生できる音楽プレーヤーアプリです。このアプリは、React.jsとTailwind CSSを使用して構築されています。',
         descriptionch: 'Earth-based Youtubesearch是一个音乐播放器应用程序，允许用户搜索歌曲并播放它们。该应用程序使用React.js和Tailwind CSS构建。',
+        descriptionko: 'Earth-based Youtubesearch는 사용자가 노래를 검색하고 재생할 수 있는 음악 플레이어 앱입니다. 이 앱은 React.js와 Tailwind CSS를 사용하여 제작되었습니다.',
         url: 'https://earthbased-youtubesearch.vercel.app/'
     },
     {
@@ -29,6 +32,7 @@ const imageSources = [
         descriptionen: 'sphere-City-Search is a simple app made by React and Babylonjs, using XR function to city search.',
         descriptionja: 'sphere-City-Searchはこれこれこういうアプリです',
         descriptionch: 'sphere-City-Search是一个简单的应用程序，允许用户搜索YouTube视频并播放它们。该应用程序使用React.js和Tailwind CSS构建。',
+        descriptionko: 'sphere-City-Search는 지도 검색을 위해 XR 기능을 사용하여 React와 Babylonjs로 만든 간단한 앱입니다.',
         url: 'https://citysearch-proto.vercel.app/'
     }
 ];
@@ -37,11 +41,34 @@ const Gallery = () => {
     const setImageData = useContext(ImageContext);
     const [hoverIndex, setHoverIndex] = useState(null);
 
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            let newHoverIndex = null;
+            if (scrollPosition < 600) {
+                newHoverIndex = 0;
+            } else if (scrollPosition >= 600 && scrollPosition < 1100) {
+                newHoverIndex = 1;
+            } else if (scrollPosition >= 1100) {
+                newHoverIndex = 2;
+            }
+            if (newHoverIndex !== hoverIndex) {
+                setHoverIndex(newHoverIndex);
+                //setHoveredImage(imageSources[newHoverIndex]);
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
         <div className='gallery-container'>
             {imageSources.map((image, index) => (
                 <div style={{ marginBottom: '6px' }}> {/* 新しくdivを追加し、各アイテムごとに余白を設定 */}
-                    <div style={{ textAlign: 'center', padding: '5px', fontWeight: 'bold', color: 'white', textDecoration: 'underline' }}>{image.alt}</div> {/* 見出しをgallery-itemの外に配置 */}
+                    <div
+                        style={{ textAlign: 'center', padding: '30px', fontWeight: 'bold', color: 'white', textDecoration: 'underline', cursor: 'pointer' }}
+                        onClick={() => window.open(image.url, '_blank')}
+                    >{image.alt}</div> {/* 見出しをgallery-itemの外に配置 */}
                     <div className='gallery-item'
                         key={index} onMouseEnter={() => {
                             setImageData(image);
@@ -64,6 +91,50 @@ const Gallery = () => {
 function App2() {
     const [hoveredImage, setHoveredImage] = useState(null);
     const [language, setLanguage] = useState('en');
+    const [hoverIndex, setHoverIndex] = useState(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            let newHoverIndex = null;
+            if (scrollPosition < 500) {
+                newHoverIndex = 0;
+            } else if (scrollPosition >= 500 && scrollPosition < 1100) {
+                newHoverIndex = 1;
+            } else if (scrollPosition >= 1100) {
+                newHoverIndex = 2;
+            }
+            if (newHoverIndex !== hoverIndex) {
+                setHoverIndex(newHoverIndex);
+                setHoveredImage(imageSources[newHoverIndex]);
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    useEffect(() => {
+        const scrollPosition = window.scrollY;
+        console.log(`scrollPosition: ${scrollPosition}`);
+    }
+    );
+
+    const descriptionSpring = useSpring({
+        to: {
+            opacity: 1,
+            transform: 'rotateY(0deg)'
+        },
+        from: {
+            opacity: 0,
+            transform: 'rotateY(180deg)'
+        },
+        reset: true,
+        config: {
+            duration: 500,// アニメーションの持続時間をミリ秒で指定
+            tension: 170,  // 張力
+            friction: 26, // 摩擦
+        }
+    });
 
     const getDescriptionByLanguage = () => {
         switch (language) {
@@ -73,16 +144,18 @@ function App2() {
                 return hoveredImage.descriptionja;
             case 'ch':
                 return hoveredImage.descriptionch;
+            case 'ko':
+                return hoveredImage.descriptionko;
             default:
                 return hoveredImage.descriptionen;
         }
     };
 
-    useEffect(() => {
+    /*useEffect(() => {
         if (hoveredImage) {
             console.log(`Hovered Image alt: ${hoveredImage.alt}, description (chinese): ${hoveredImage.descriptionch}`);
         }
-    }, [hoveredImage]);
+    }, [hoveredImage]);*/
 
     return (
         <ImageContext.Provider value={setHoveredImage}>
@@ -91,11 +164,14 @@ function App2() {
                 <Gallery />
                 {hoveredImage && (
                     <div className="description-box">
-                        <div>{getDescriptionByLanguage()}</div>
-                        <div style={{ marginTop: '10px' }}>
-                            <button onClick={() => setLanguage('en')}>English</button>
-                            <button onClick={() => setLanguage('ja')}>日本語</button>
-                            <button onClick={() => setLanguage('ch')}>中文</button>
+                        <div className='text-content'>
+                            <div className="button-container">
+                                <button className="language-button" onClick={() => setLanguage('en')}>en</button>
+                                <button className="language-button" onClick={() => setLanguage('ja')}>日</button>
+                                <button className="language-button" onClick={() => setLanguage('ch')}>中</button>
+                                <button className="language-button" onClick={() => setLanguage('ko')}>한</button>
+                            </div>
+                            <animated.div style={descriptionSpring}>{getDescriptionByLanguage()}</animated.div>
                         </div>
                     </div>
                 )}
